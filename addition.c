deba524a67ab2dfbd198b2a7467ec2acf66c5250
xxDEFIBRILATORxx <93367777+xxDEFIBRILATORxx@users.noreply.github.com>
2021-11-16 14:32:16 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int a , b;
+ 
+ printf("the number a = ");
+ scanf("%d" , &a );
+
+ printf("the number b = ");
+ scanf("%d" , &b);
+
+ printf("the value a + b = %d ", (a + b) );
+
+ return 0;
+}
