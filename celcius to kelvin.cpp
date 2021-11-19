002c718b3ed2b176f5c2e2688ff1b9fe8fa7b059
xxDEFIBRILATORxx <93367777+xxDEFIBRILATORxx@users.noreply.github.com>
2021-11-19 12:28:41 +0530

    Add files via upload

---
+++ celcius to kelvin.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+   int c;
+   printf("the value of c in celcius = ");
+   scanf("%d" , &c);
+   
+   printf("the value is = %f " , ((9/5)*c+32));
+   
+   return 0;
+}
+
